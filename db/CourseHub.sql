3b28dfdb62626c65f06c7dcf188372067bc41f70
SpaceRage <66397938+SpaceRage@users.noreply.github.com>
2023-04-17 17:04:24 -0400

    Added sample data insert statements.

---
+++ db/CourseHub.sql
@@ -0,0 +1,195 @@
+-- This file is to bootstrap a database for the CS3200 project. 
+
+-- Create a new database.  You can change the name later.  You'll
+-- need this name in the FLASK API file(s),  the AppSmith 
+-- data source creation.
+create database if not exists CourseHub;
+
+-- Via the Docker Compose file, a special user called webapp will 
+-- be created in MySQL. We are going to grant that user 
+-- all privilages to the new database we just created. 
+-- TODO: If you changed the name of the database above, you need 
+-- to change it here too.
+grant all privileges on CourseHub.* to 'webapp'@'%';
+flush privileges;
+
+-- Move into the database we just created.
+-- TODO: If you changed the name of the database above, you need to
+-- change it here too. 
+use CourseHub;
+
+-- Student
+CREATE TABLE IF NOT EXISTS Student
+(
+    Student_ID int PRIMARY KEY,
+    FName      varchar(25) NOT NULL,
+    LName      varchar(25) NOT NULL,
+    Email      varchar(50),
+    Phone      varchar(20),
+    SSN        varchar(10)
+);
+
+-- Enrollment_Order
+CREATE TABLE IF NOT EXISTS EnrollmentOrder
+(
+    EnrollmentOrder_ID int PRIMARY KEY,
+    Student_ID         int      NOT NULL,
+    Order_Date         datetime NOT NULL DEFAULT current_timestamp,
+    CONSTRAINT FK_1 FOREIGN KEY (Student_ID)
+        REFERENCES Student (Student_ID)
+        ON DELETE CASCADE
+        ON UPDATE CASCADE
+);
+
+-- School
+CREATE TABLE IF NOT EXISTS School
+(
+    School_ID   int PRIMARY KEY,
+    School_Name varchar(100) NOT NULL,
+    City        varchar(50)  NOT NULL,
+    State       varchar(50)  NOT NULL,
+    Zipcode     varchar(10)  NOT NULL
+);
+
+-- Academic Officer
+CREATE TABLE IF NOT EXISTS AcademicOfficer
+(
+    Officer_ID   int PRIMARY KEY,
+    FName        varchar(25) NOT NULL,
+    LName        varchar(25) NOT NULL,
+    School_ID    int         NOT NULL,
+    Years_Worked int,
+    CONSTRAINT FK_2 FOREIGN KEY (School_ID)
+        REFERENCES School (School_ID)
+        ON DELETE RESTRICT
+        ON UPDATE CASCADE
+);
+
+-- Department
+CREATE TABLE IF NOT EXISTS Department
+(
+    Department_ID   int PRIMARY KEY,
+    Department_Name varchar(50) NOT NULL,
+    School_ID       int         NOT NULL,
+    CONSTRAINT FK_3 FOREIGN KEY (School_ID)
+        REFERENCES School (School_ID)
+        ON DELETE CASCADE
+        ON UPDATE CASCADE
+);
+
+-- Course
+CREATE TABLE IF NOT EXISTS Course
+(
+    Course_ID     int PRIMARY KEY,
+    Course_Name   varchar(50) NOT NULL,
+    Difficulty    int,
+    Department_ID int         NOT NULL,
+    CONSTRAINT FK_5 FOREIGN KEY (Department_ID)
+        REFERENCES Department (Department_ID)
+        ON DELETE CASCADE
+        ON UPDATE CASCADE
+);
+
+-- Section
+CREATE TABLE IF NOT EXISTS Section
+(
+    Course_ID  int,
+    Section_ID int,
+    InPerson   boolean     NOT NULL,
+    City       varchar(50),
+    State      varchar(50),
+    Zipcode    varchar(10),
+    Capacity   int         NOT NULL,
+    Prof_ID    int         NOT NULL,
+    Price float(5, 2) NOT NULL,
+    PRIMARY KEY (Course_ID, Section_ID),
+    CONSTRAINT FK_6 FOREIGN KEY (Course_ID)
+        REFERENCES Course (Course_ID)
+        ON DELETE CASCADE
+        ON UPDATE CASCADE,
+    CONSTRAINT FK_7 FOREIGN KEY (Prof_ID)
+        REFERENCES Professor (Prof_ID)
+        ON DELETE CASCADE
+        ON UPDATE CASCADE
+);
+
+-- Professor
+CREATE TABLE IF NOT EXISTS Professor
+(
+    Prof_ID      int PRIMARY KEY,
+    FName        varchar(25) NOT NULL,
+    LName        varchar(25) NOT NULL,
+    School_ID    int         NOT NULL,
+    Years_Worked int,
+    CONSTRAINT FK_4 FOREIGN KEY (School_ID)
+        REFERENCES School (School_ID)
+        ON DELETE RESTRICT
+        ON UPDATE CASCADE
+);
+
+-- Textbook
+CREATE TABLE IF NOT EXISTS Textbook
+(
+    ISBN        int PRIMARY KEY,
+    Name        varchar(100) NOT NULL,
+    Price       float(5, 2),
+    AuthorFName varchar(25),
+    AuthorLName varchar(25),
+    Edition     varchar(25)
+);
+
+-- Review
+CREATE TABLE IF NOT EXISTS Review
+(
+    Student_ID     int,
+    Course_ID      int,
+    Section_ID     int,
+    Review_Date    datetime NOT NULL DEFAULT current_timestamp,
+    Review_Content varchar(500),
+    Rating         int      NOT NULL,
+    PRIMARY KEY (Student_ID, Course_ID, Section_ID),
+    CONSTRAINT FK_8 FOREIGN KEY (Student_ID)
+        REFERENCES Student (Student_ID)
+        ON DELETE CASCADE
+        ON UPDATE CASCADE,
+    CONSTRAINT FK_9 FOREIGN KEY (Course_ID, Section_ID)
+        REFERENCES Section (Course_ID, Section_ID)
+        ON DELETE CASCADE
+        ON UPDATE CASCADE
+)
+
+-- EnrollmentOrderDetail
+CREATE TABLE IF NOT EXISTS EnrollmentOrderDetail
+(
+    EnrollmentOrder_ID int,
+    Course_ID          int,
+    Section_ID         int,
+    Price              float(5, 2) NOT NULL,
+    EnrolledSemester varchar(10) NOT NULL,
+    EnrolledYear year NOT NULL,
+    PRIMARY KEY (EnrollmentOrder_ID, Course_ID, Section_ID),
+    CONSTRAINT FK_10 FOREIGN KEY (EnrollmentOrder_ID)
+        REFERENCES EnrollmentOrder (EnrollmentOrder_ID)
+        ON DELETE CASCADE
+        ON UPDATE CASCADE,
+    CONSTRAINT FK_11 FOREIGN KEY (Course_ID, Section_ID)
+        REFERENCES Section (Course_ID, Section_ID)
+        ON DELETE CASCADE
+        ON UPDATE CASCADE
+)
+
+-- CourseTextbook
+CREATE TABLE IF NOT EXISTS CourseTextbook
+(
+    ISBN      int,
+    Course_ID int,
+    PRIMARY KEY (ISBN, COURSE_ID),
+    CONSTRAINT FK_12 FOREIGN KEY (ISBN)
+        REFERENCES Textbook (ISBN)
+        ON DELETE CASCADE
+        ON UPDATE CASCADE,
+    CONSTRAINT FK_13 FOREIGN KEY (Course_ID)
+        REFERENCES Course (Course_ID)
+        ON DELETE CASCADE
+        ON UPDATE CASCADE
+);
---
+++ db/z_School.sql
@@ -0,0 +1,22 @@
+use CourseHub;
+
+INSERT INTO School(School_ID,School_Name,City,State,Zipcode) VALUES (1,'Tohoku University','Bismarck','North Dakota',58505);
+INSERT INTO School(School_ID,School_Name,City,State,Zipcode) VALUES (2,'University of Bunkyo','Cincinnati','Ohio',45243);
+INSERT INTO School(School_ID,School_Name,City,State,Zipcode) VALUES (3,'Kuwait International Law School','Philadelphia','Pennsylvania',19104);
+INSERT INTO School(School_ID,School_Name,City,State,Zipcode) VALUES (4,'Mahatma Gandhi University Meghalaya','Orlando','Florida',32830);
+INSERT INTO School(School_ID,School_Name,City,State,Zipcode) VALUES (5,'European School of Economics','Monticello','Minnesota',55590);
+INSERT INTO School(School_ID,School_Name,City,State,Zipcode) VALUES (6,'Zhejiang University','Fairfax','Virginia',22036);
+INSERT INTO School(School_ID,School_Name,City,State,Zipcode) VALUES (7,'Instituto Universitario de Ciencias de la Salud Fundación H.A. Barceló','Saint Louis','Missouri',63121);
+INSERT INTO School(School_ID,School_Name,City,State,Zipcode) VALUES (8,'Hastings College','Chico','California',95973);
+INSERT INTO School(School_ID,School_Name,City,State,Zipcode) VALUES (9,'Islamia University of Bahawalpur','Baltimore','Maryland',21281);
+INSERT INTO School(School_ID,School_Name,City,State,Zipcode) VALUES (10,'Instituto Superior de Ciências do Trabalho e da Empresa','Madison','Wisconsin',53705);
+INSERT INTO School(School_ID,School_Name,City,State,Zipcode) VALUES (11,'Gauhati University','New York City','New York',10270);
+INSERT INTO School(School_ID,School_Name,City,State,Zipcode) VALUES (12,'Wentworth Institute of Technology','Grand Forks','North Dakota',58207);
+INSERT INTO School(School_ID,School_Name,City,State,Zipcode) VALUES (13,'Alkharj University','Chicago','Illinois',60636);
+INSERT INTO School(School_ID,School_Name,City,State,Zipcode) VALUES (14,'State University of New York Health Sience Centre Syracuse','Las Vegas','Nevada',89125);
+INSERT INTO School(School_ID,School_Name,City,State,Zipcode) VALUES (15,'Royal Academy of Music','Scranton','Pennsylvania',18505);
+INSERT INTO School(School_ID,School_Name,City,State,Zipcode) VALUES (16,'Hobart and William Smith Colleges','Minneapolis','Minnesota',55417);
+INSERT INTO School(School_ID,School_Name,City,State,Zipcode) VALUES (17,'Australian Correspondence Schools','Fresno','California',93704);
+INSERT INTO School(School_ID,School_Name,City,State,Zipcode) VALUES (18,'Misamis University','Little Rock','Arkansas',72222);
+INSERT INTO School(School_ID,School_Name,City,State,Zipcode) VALUES (19,'Universidad Los Andes','Fort Pierce','Florida',34949);
+INSERT INTO School(School_ID,School_Name,City,State,Zipcode) VALUES (20,'Chiang Mai University','Cleveland','Ohio',44118);
