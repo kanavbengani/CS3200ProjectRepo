4ab0c2132d3abe65a379e2c5b8594df07d69a0cf
SpaceRage <66397938+SpaceRage@users.noreply.github.com>
2023-04-17 19:05:13 -0400

    Adjusted info returned from API.

--- flask-app/src/students/students.py
+++ flask-app/src/students/students.py
@@ -5,16 +5,16 @@ from src import db
 
 students = Blueprint('students', __name__)
 
+# Returns all courses.
+
 
 @students.route('/courses', methods=['GET'])
-def get_customers():
+def get_courses():
     cursor = db.get_db().cursor()
-    cursor.execute("""SELECT C.Course_Name,
-       D.Department_Name,
-       Sc.School_Name,
-       P.FName as 'Professor First Name',
-       P.LName as 'Professor Last Name',
-       C.Difficulty
+    cursor.execute("""SELECT C.Course_Name as 'Course',
+       D.Department_Name as 'Department',
+       Sc.School_Name as 'School',
+       C.Difficulty as 'Difficulty'
 FROM Course C
          JOIN Department D using (Department_ID)
          JOIN School Sc using (School_ID)
@@ -30,11 +30,22 @@ FROM Course C
     the_response.mimetype = 'application/json'
     return the_response
 
+# Returns information for a specific course.
 
-@students.route('/desccourses', methods=['GET'])
-def desc_courses():
+
+@students.route('/courses/<course_id>', methods=['GET'])
+def get_courses_by_id(course_id):
     cursor = db.get_db().cursor()
-    cursor.execute('desc Course;')
+    cursor.execute("""SELECT DISTINCT C.Course_Name as 'Course',
+       D.Department_Name as 'Department',
+       Sc.School_Name as 'School',
+       C.Difficulty as 'Difficulty'
+FROM Course C
+            JOIN Department D using (Department_ID)
+            JOIN School Sc using (School_ID)
+            JOIN Section Se using (Course_ID)
+            JOIN Professor P using (Prof_ID)
+WHERE C.Course_ID = %s;""", course_id)
     row_headers = [x[0] for x in cursor.description]
     json_data = []
     theData = cursor.fetchall()
@@ -44,3 +55,34 @@ def desc_courses():
     the_response.status_code = 200
     the_response.mimetype = 'application/json'
     return the_response
+
+# Returns information for a specific section of a course.
+
+
+@students.route('/courses/<course_id>/<section_id>', methods=['GET'])
+def get_courses_by_id_and_section_id(course_id, section_id):
+    cursor = db.get_db().cursor()
+    cursor.execute(f"""SELECT C.Course_Name as 'Course Name',
+       Se.Section_ID as 'Section',
+       Sc.School_Name as 'School Name',
+       Se.InPerson as 'In person?',
+       P.FName as 'Professor First Name',
+       P.LName as 'Professor Last Name',
+       C.Difficulty as 'Difficulty'
+FROM Course C
+            JOIN Department D using (Department_ID)
+            JOIN School Sc using (School_ID)
+            JOIN Section Se using (Course_ID)
+            JOIN Professor P using (Prof_ID)
+WHERE C.Course_ID = {course_id} AND Se.Section_ID = {section_id};""")
+    row_headers = [x[0] for x in cursor.description]
+    json_data = []
+    theData = cursor.fetchall()
+    for row in theData:
+        json_data.append(dict(zip(row_headers, row)))
+    the_response = make_response(jsonify(json_data))
+    the_response.status_code = 200
+    the_response.mimetype = 'application/json'
+    return the_response
+
+# Returns all sections of a specific course.
