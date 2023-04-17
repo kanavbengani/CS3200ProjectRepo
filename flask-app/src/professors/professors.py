0cb920166253caccd6344793d43de2fc46872056
Pranav Rana <ranapranav00@gmail.com>
2023-04-17 17:14:13 -0400

    Teacher -> Professor

---
+++ flask-app/src/professors/professors.py
@@ -0,0 +1,21 @@
+from flask import Blueprint, request, jsonify, make_response
+import json
+from src import db
+
+
+professors = Blueprint('professors', __name__)
+
+@professors.route('/professors', methods=['GET'])
+def get_customers():
+    cursor = db.get_db().cursor()
+    cursor.execute('select company, last_name,\
+        first_name, job_title, business_phone from customers')
+    row_headers = [x[0] for x in cursor.description]
+    json_data = []
+    theData = cursor.fetchall()
+    for row in theData:
+        json_data.append(dict(zip(row_headers, row)))
+    the_response = make_response(jsonify(json_data))
+    the_response.status_code = 200
+    the_response.mimetype = 'application/json'
+    return the_response
