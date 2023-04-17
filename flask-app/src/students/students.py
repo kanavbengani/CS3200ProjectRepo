5cbb8b793abe0d28127948aa9fa618e87e2c4743
SpaceRage <66397938+SpaceRage@users.noreply.github.com>
2023-04-17 19:38:14 -0400

    Formatting of In Person attr of Section

--- flask-app/src/students/students.py
+++ flask-app/src/students/students.py
@@ -65,7 +65,7 @@ def get_courses_by_id_and_section_id(course_id, section_id):
     cursor.execute(f"""SELECT C.Course_Name as 'Course Name',
        Se.Section_ID as 'Section',
        Sc.School_Name as 'School Name',
-       Se.InPerson as 'In person?',
+       IF(Se.InPerson='1',"Yes","No") AS 'In Person?',
        P.FName as 'Professor First Name',
        P.LName as 'Professor Last Name',
        C.Difficulty as 'Difficulty'
@@ -86,3 +86,86 @@ WHERE C.Course_ID = {course_id} AND Se.Section_ID = {section_id};""")
     return the_response
 
 # Returns all sections of a specific course.
+
+
+@students.route('/courses/<course_id>/sections', methods=['GET'])
+def get_sections_by_course_id(course_id):
+    cursor = db.get_db().cursor()
+    cursor.execute(f"""SELECT Se.Section_ID as 'Section',
+       IF(Se.InPerson='1',"Yes","No") AS 'InPerson?',
+       P.FName as 'Professor First Name',
+       P.LName as 'Professor Last Name',
+       C.Difficulty as 'Difficulty'
+FROM Course C
+            JOIN Department D using (Department_ID)
+            JOIN School Sc using (School_ID)
+            JOIN Section Se using (Course_ID)
+            JOIN Professor P using (Prof_ID)
+WHERE C.Course_ID = {course_id};""")
+    row_headers = [x[0] for x in cursor.description]
+    json_data = []
+    theData = cursor.fetchall()
+    for row in theData:
+        json_data.append(dict(zip(row_headers, row)))
+    the_response = make_response(jsonify(json_data))
+    the_response.status_code = 200
+    the_response.mimetype = 'application/json'
+    return the_response
+
+# Returns all reviews of a specific course.
+
+
+@students.route('/courses/<course_id>/reviews', methods=['GET'])
+def get_reviews_by_course_id(course_id):
+    cursor = db.get_db().cursor()
+    cursor.execute(f"""SELECT R.Rating as 'Rating',
+       R.Review_Content as 'Comment',
+       R.Review_Date as 'Date',
+       R.Course_ID as 'Course ID',
+       R.Section_ID as 'Section ID',
+       R.Student_ID as 'Student ID'
+FROM Course C
+            JOIN Department D using (Department_ID)
+            JOIN School Sc using (School_ID)
+            JOIN Section Se using (Course_ID)
+            JOIN Professor P using (Prof_ID)
+            JOIN Review R using (Section_ID)
+WHERE C.Course_ID = {course_id};""")
+    row_headers = [x[0] for x in cursor.description]
+    json_data = []
+    theData = cursor.fetchall()
+    for row in theData:
+        json_data.append(dict(zip(row_headers, row)))
+    the_response = make_response(jsonify(json_data))
+    the_response.status_code = 200
+    the_response.mimetype = 'application/json'
+    return the_response
+
+# Returns all reviews of a specific section.
+
+
+@students.route('/courses/<course_id>/<section_id>/reviews', methods=['GET'])
+def get_reviews_by_course_id_and_section_id(course_id, section_id):
+    cursor = db.get_db().cursor()
+    cursor.execute(f"""SELECT R.Rating as 'Rating',
+       R.Review_Content as 'Comment',
+       R.Review_Date as 'Date',
+       R.Course_ID as 'Course ID',
+       R.Section_ID as 'Section ID',
+       R.Student_ID as 'Student ID'
+FROM Course C
+            JOIN Department D using (Department_ID)
+            JOIN School Sc using (School_ID)
+            JOIN Section Se using (Course_ID)
+            JOIN Professor P using (Prof_ID)
+            JOIN Review R using (Section_ID)
+WHERE C.Course_ID = {course_id} AND Se.Section_ID = {section_id};""")
+    row_headers = [x[0] for x in cursor.description]
+    json_data = []
+    theData = cursor.fetchall()
+    for row in theData:
+        json_data.append(dict(zip(row_headers, row)))
+    the_response = make_response(jsonify(json_data))
+    the_response.status_code = 200
+    the_response.mimetype = 'application/json'
+    return the_response
